46aa862caa9ae411e10bbc4d676850a509425575
TarasekM <rezkth@gmail.com>
2019-01-05 21:46:14 +0100

    Implement initialization and movement of player.

--- src/main/java/com/tarasek/mateusz/Player.java
+++ src/main/java/com/tarasek/mateusz/Player.java
@@ -1,12 +1,14 @@
 package com.tarasek.mateusz;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Player extends Sprite implements SharedVariables{
 
     private final int START_X = 240;
     private final int START_Y = 520;
     private final int width = 64;
+    private final int heigth = 64;
 
     private final String playerSprite = "src/img/player.png";
 
@@ -17,6 +19,34 @@ public class Player extends Sprite implements SharedVariables{
     private void initPLayer(){
         ImageIcon imageIcon = new ImageIcon(playerSprite);
         setImage(imageIcon);
+        setX(START_X);
+        setY(START_Y);
+    }
 
+    public void move(){
+        int x, y;
+
+        Point mousePosition = MouseInfo.getPointerInfo().getLocation();
+        x = mousePosition.x;
+        y = mousePosition.y;
+
+        if (x >= BOARD_WIDTH - width){
+            x = BOARD_WIDTH - width;
+        }
+
+        if (y >= BOARD_HEIGTH - heigth){
+            y = BOARD_HEIGTH - heigth;
+        }
+
+        if (x <= width){
+            x = width;
+        }
+
+        if(y <= heigth){
+            x = heigth;
+        }
+
+        this.x = x;
+        this.y = y;
     }
 }
