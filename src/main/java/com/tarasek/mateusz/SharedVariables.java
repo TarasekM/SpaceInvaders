19e45abb48582b033a1a3a77fff2b9b151db3e65
TarasekM <rezkth@gmail.com>
2019-01-08 22:52:58 +0100

    Add shared variable.

--- src/main/java/com/tarasek/mateusz/SharedVariables.java
+++ src/main/java/com/tarasek/mateusz/SharedVariables.java
@@ -2,5 +2,6 @@ package com.tarasek.mateusz;
 
 public interface SharedVariables {
     public static final int BOARD_WIDTH = 480;
-    public static final int BOARD_HEIGTH = 640;
+    public static final int BOARD_HEIGHT = 640;
+    public static final int  DELAY = 17;
 }
