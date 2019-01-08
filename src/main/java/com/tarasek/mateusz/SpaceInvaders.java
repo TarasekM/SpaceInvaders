22a0d16f04d705e8dc9e4c6a7655a047ee4b5420
TarasekM <rezkth@gmail.com>
2019-01-08 16:31:59 +0100

    Add some basic for gui

--- src/main/java/com/tarasek/mateusz/SpaceInvaders.java
+++ src/main/java/com/tarasek/mateusz/SpaceInvaders.java
@@ -1,18 +1,28 @@
 package com.tarasek.mateusz;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class SpaceInvaders extends JFrame {
+public class SpaceInvaders extends JFrame implements SharedVariables{
 
     public SpaceInvaders(){
         init();
     }
 
     private void init(){
-
+        setTitle("Space Invaders");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(BOARD_WIDTH, BOARD_HEIGTH);
+        setLocationRelativeTo(null);
+        setResizable(false);
     }
 
+
     public static void main(String[] args) {
-	// write your code here
+
+        EventQueue.invokeLater(() -> {
+            SpaceInvaders spaceInvaders = new SpaceInvaders();
+            spaceInvaders.setVisible(true);
+        });
     }
 }
