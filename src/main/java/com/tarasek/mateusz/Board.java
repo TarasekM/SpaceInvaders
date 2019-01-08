aabd037b51d25bb1771656c8607c95e28922c6cc
TarasekM <rezkth@gmail.com>
2019-01-08 16:32:30 +0100

    Add basic Board class.

---
+++ src/main/java/com/tarasek/mateusz/Board.java
@@ -0,0 +1,15 @@
+package com.tarasek.mateusz;
+
+import javax.swing.*;
+
+public class Board extends JPanel implements Runnable,SharedVariables {
+
+    @Override
+    public void run() {
+        while (true) {
+
+            repaint();
+
+        }
+    }
+}
