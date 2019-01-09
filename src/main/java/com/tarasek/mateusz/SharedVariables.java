e1ec8c1a00c9f5b6786c938b8b8d4496edee12cb
TarasekM <rezkth@gmail.com>
2019-01-09 20:26:48 +0100

    Move positioning variables from Shot to SharedVariables.

--- src/main/java/com/tarasek/mateusz/SharedVariables.java
+++ src/main/java/com/tarasek/mateusz/SharedVariables.java
@@ -4,4 +4,9 @@ public interface SharedVariables {
     public static final int BOARD_WIDTH = 480;
     public static final int BOARD_HEIGHT = 640;
     public static final int DELAY = 17;
+    public static final int SHOT_WIDTH_SPACE = 19;
+    public static final int SHOT_HEIGHT_SPACE = 2;
+    public static final int SHOT_SPEED = 4;
+    public static final int BOMB_SPEED = 4;
 }
+
--- src/main/java/com/tarasek/mateusz/Shot.java
+++ src/main/java/com/tarasek/mateusz/Shot.java
@@ -2,11 +2,9 @@ package com.tarasek.mateusz;
 
 import javax.swing.*;
 
-public class Shot extends Sprite{
+public class Shot extends Sprite implements SharedVariables{
 
     private final String shotSprite = "src/Sprites/DODO_SHOT.png";
-    private final int widthSpace = 19;
-    private final int heightSpace = 2;
 
     public Shot(int x, int y){
         initShot(x, y);
@@ -15,7 +13,12 @@ public class Shot extends Sprite{
     private void initShot(int x, int y){
         ImageIcon imageIcon = new ImageIcon(shotSprite);
         setImage(imageIcon.getImage());
-        setX(x + widthSpace);
-        setY(y + heightSpace);
+        setX(x + SHOT_WIDTH_SPACE);
+        setY(y + SHOT_HEIGHT_SPACE);
     }
+
+    public void moveUpwards(){
+        setY(getY() - SHOT_SPEED);
+    }
+
 }
