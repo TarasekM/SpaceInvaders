445db91a0d0b24007b5ee775100f5aa9758edc29
TarasekM <rezkth@gmail.com>
2019-01-09 20:20:31 +0100

    Changed behaviour of move function.

--- src/main/java/com/tarasek/mateusz/Alien.java
+++ src/main/java/com/tarasek/mateusz/Alien.java
@@ -21,8 +21,8 @@ public class Alien extends Sprite{
     }
 
     public void move(int x, int y){
-        this.x += x;
-        this.y += y;
+        this.x = x;
+        this.y = y;
     }
 
     public Bomb getBomb() {
