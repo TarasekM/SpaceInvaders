411480d53697d4c471cc217dc516b12699f820b7
TarasekM <rezkth@gmail.com>
2019-01-10 18:53:49 +0100

    Changes in test to make sure they are viable.

--- src/test/java/com/tarasek/mateusz/ShotTest.java
+++ src/test/java/com/tarasek/mateusz/ShotTest.java
@@ -11,19 +11,19 @@ public class ShotTest implements SharedVariables{
 
     @BeforeEach
     void setShot(){
-        shot = new Shot(10,20);
+        shot = new Shot(25, 50);
     }
 
     @Test
     void init(){
         assertNotNull(shot.getImage());
-        assertEquals(10 + SHOT_WIDTH_SPACE, shot.getX());
-        assertEquals(20 + SHOT_HEIGHT_SPACE, shot.getY());
+        assertEquals(25 + SHOT_WIDTH_SPACE, shot.getX());
+        assertEquals(50 + SHOT_HEIGHT_SPACE, shot.getY());
     }
 
     @Test
     void moveUpwards(){
         shot.moveUpwards();
-        assertEquals(20 - SHOT_SPEED + SHOT_HEIGHT_SPACE, shot.getY());
+        assertEquals(50 - SHOT_SPEED + SHOT_HEIGHT_SPACE, shot.getY());
     }
 }
