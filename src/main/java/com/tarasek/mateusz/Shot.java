af9bc88a7b5cee6584e27376fd11580b5a281663
TarasekM <rezkth@gmail.com>
2019-01-10 18:59:03 +0100

    Shot is an circular object, but Sprite is rectangular. Needed to add circular collider.

--- src/main/java/com/tarasek/mateusz/Shot.java
+++ src/main/java/com/tarasek/mateusz/Shot.java
@@ -1,8 +1,12 @@
 package com.tarasek.mateusz;
 
 import javax.swing.*;
+import java.awt.geom.Ellipse2D;
 
 public class Shot extends Sprite implements SharedVariables{
+
+    private final int RADIUS = 5;
+    private Ellipse2D.Float collider;
 
     private final String shotSprite = "src/Sprites/DODO_SHOT.png";
 
@@ -15,10 +19,16 @@ public class Shot extends Sprite implements SharedVariables{
         setImage(imageIcon.getImage());
         setX(x + SHOT_WIDTH_SPACE);
         setY(y + SHOT_HEIGHT_SPACE);
+        collider = new Ellipse2D.Float(this.x-1, this.y-1, RADIUS*2, 2*RADIUS);
     }
 
     public void moveUpwards(){
-        setY(getY() - SHOT_SPEED);
+        setY((int)getY() - SHOT_SPEED);
+        collider.y = (int)getY() - SHOT_SPEED;
+    }
+
+    public Ellipse2D.Float getCollider(){
+        return collider;
     }
 
 }
