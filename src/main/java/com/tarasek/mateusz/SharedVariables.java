61ac011734ee9f23a852f962dc1a95fa5dd74740
TarasekM <rezkth@gmail.com>
2019-01-13 13:26:30 +0100

    Delete unnecessary variable. Add AlienSpeed variable.

--- src/main/java/com/tarasek/mateusz/SharedVariables.java
+++ src/main/java/com/tarasek/mateusz/SharedVariables.java
@@ -3,10 +3,10 @@ package com.tarasek.mateusz;
 public interface SharedVariables {
     public static final int BOARD_WIDTH = 480;
     public static final int BOARD_HEIGHT = 640;
-    public static final int DELAY = 17;
     public static final int SHOT_WIDTH_SPACE = 19;
     public static final int SHOT_HEIGHT_SPACE = 2;
     public static final int SHOT_SPEED = 3;
     public static final int BOMB_SPEED = 4;
+    public static final int ALIEN_SPEED = 1;
 }
 
